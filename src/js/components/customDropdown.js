7692801d6ce1fe87fcf4d8a090b666ecb07bb0c2
Maksym <maksymRozhko7@gmail.com>
2022-11-11 19:01:00 +0200

    window dropdown close

--- src/js/components/customDropdown.js
+++ src/js/components/customDropdown.js
@@ -7,6 +7,13 @@ const customDropdown = (triggerSelector, activeClass) => {
             btn.parentElement.classList.toggle(activeClass);
         });
     }
+
+    window.addEventListener('click', ({ target }) => {
+        if (!target.closest(triggerSelector)) {
+            btn.nextElementSibling.classList.add('d-none');
+            btn.parentElement.classList.remove(activeClass);
+        }
+    });
 }
 
 customDropdown('.profile .profile__wrap', 'profile--show');

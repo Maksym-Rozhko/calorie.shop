12406d7684f3e2f5aac92805f22f1b63f3836e4b
Maksym <maksymRozhko7@gmail.com>
2022-11-14 14:45:51 +0200

    feat: order modal, fixes

--- src/js/components/customScripts.js
+++ src/js/components/customScripts.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
     $('.js-example-basic-single').select2();
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const closeModalBtns = document.querySelectorAll('.graph-modal-close');
+    const graphModalOverflow = document.querySelector('.graph-modal');
+
+    if (closeModalBtns) {
+        closeModalBtns.forEach(btn => {
+            btn.addEventListener('click', () => graphModalOverflow.click());
+        });
+    }
+});

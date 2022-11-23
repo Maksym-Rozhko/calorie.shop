15576757e027777e1f613a8d0f039fd49cfe18b6
Maksym <maksymRozhko7@gmail.com>
2022-11-23 10:56:51 +0200

    balance page

--- src/js/components/customScripts.js
+++ src/js/components/customScripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('.js-example-basic-single').select2();
+    // $('.js-example-basic-single').select2();
 
     $("#slider-range").slider({
         range: true,

ed1791501c82f196b1cd4aa02467b62c62114f87
Maksym <maksymRozhko7@gmail.com>
2022-11-25 16:35:57 +0200

    auth login/signup, preloader

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,6 +1,6 @@
 // import headerScrollFixed from './components/fixedHeader';
-// import customScripts from './components/customScripts';
-// import customCatalogTabs from './components/customTabs';
-// import customDropdown from './components/customDropdown';
-// import clipboardCopy from './components/clipboardCopy';
-import clipboardCopy from './components/preloader';
+import customScripts from './components/customScripts';
+import customCatalogTabs from './components/customTabs';
+import customDropdown from './components/customDropdown';
+import clipboardCopy from './components/clipboardCopy';
+import preloader from './components/preloader';
